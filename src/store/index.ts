c6d869878bfa471655043fee7eccb4fc162adad9
zhazhahui <1376393028@qq.com>
2022-12-19 13:03:06 +0800

    添加更新，删除tag方法

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,13 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import clone from "@/utils/clone";
+import router from '@/router';
 const localStorageRecordName = 'recordList';
 const localStorageTagName = 'tagList';
 
 
 Vue.use(Vuex);
 
-type rootState ={
+type rootState = {
   recordList: RecordItem[]
   tagList: Tag[]
   currentTag?: Tag
@@ -38,20 +39,39 @@ const store = new Vuex.Store({
     },
     createTag(state, name) {
       const names = state.tagList.map((item) => item.name)
-        if(names.indexOf(name) >= 0) {
-          alert('创建的标签已存在');
-          return
-         }
-        state.tagList.push({id: Date.now().toString(), name});
-        store.commit('saveTags');
-        alert('创建成功');
+      if (names.indexOf(name) >= 0) {
+        alert('创建的标签已存在');
+        return
+      }
+      state.tagList.push({ id: Date.now().toString(), name });
+      store.commit('saveTags');
+      alert('创建成功');
     },
     saveTags(state) {
       localStorage.setItem(localStorageTagName, JSON.stringify(state.tagList));
     },
     setCurrentTag(state, id) {
       state.currentTag = state.tagList.filter(item => item.id === id)[0]
-    }
+    },
+    updateTag(state, name) {
+      let tag = state.currentTag;
+      if (tag) {
+        const names = state.tagList.map((item) => item.name)
+        if (names.indexOf(name) >= 0) {
+          console.log('标签名重复');
+        } else {
+          tag.name = name;
+          store.commit('saveTags');
+        }
+      }
+    },
+    removeTag(state, id) {
+      state.tagList = state.tagList.filter(item => item.id !== id);
+      store.commit('saveTags');
+      alert('删除成功');
+      state.currentTag = undefined;
+      router.back();
+    },
   }
 })
 
