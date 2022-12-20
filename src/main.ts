e0850ff1c71d948ae74628a14ea481867edc1e04
zhazhahui <1376393028@qq.com>
2022-12-20 19:22:00 +0800

    保证用户用手机打开页面，否则提示

--- src/main.ts
+++ src/main.ts
@@ -22,3 +22,10 @@ new Vue({
 }).$mount('#app')
 
 
+if(document.documentElement.clientWidth > 500) {
+  alert('请使用手机打开页面,以保证浏览效果');
+  const img = document.createElement('img');
+  img.src = '/qrcode.png';
+  img.setAttribute('style', 'width:200px; height:200px;position: fixed; top:50%; left:50%; transform: translate(-50%, -100%); box-shadow: 0 0 10px rgba(0, 0, 0, 0.2)')
+  document.body.append(img);
+}
